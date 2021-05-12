9e044ed332cda0631557aa914bc56e9c13b6b6a5
smallrookie <1543666575@qq.com>
2021-05-12 22:25:37 +0800

    完成数据库设计

--- Flask/qa_flask/app.py
+++ Flask/qa_flask/app.py
@@ -1,6 +1,257 @@
+from datetime import datetime
+
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+import constants
 
 app = Flask(__name__, static_folder='assets')
+# 数据库配置URI
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:0321@localhost/flask_qa'
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    """ 用户模型 """
+    __tablename__ = 'accounts_user'
+    # 主键
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    # 用户名
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    # 用户昵称
+    nickname = db.Column(db.String(64))
+    # 登录密码
+    password = db.Column(db.String(256), nullable=False)
+    # 用户头像地址
+    avatar = db.Column(db.String(256))
+    # 用户状态
+    status = db.Column(db.SmallInteger, default=constants.UserStatus.USER_ACTIVE.value, comment='用户状态')
+    # 是否为超级管理员
+    is_super = db.Column(db.SmallInteger, default=constants.UserRole.COMMON.value)
+    # 创建时间
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    # 最后修改时间
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+
+class UserProfile(db.Model):
+    """ 用户详细信息 """
+    __tablename__ = 'accounts_user_profile'
+    # 主键
+    id = db.Column(db.Integer, primary_key=True)
+    # 用户真实姓名
+    real_name = db.Column(db.String(64), unique=True, nullable=False)
+    # 个性签名
+    maxim = db.Column(db.String(128))
+    # 性别
+    gender = db.Column(db.String(16))
+    # 地址
+    address = db.Column(db.String(256))
+    # 创建时间
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    # 最后修改时间
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    # 外键-关联用户模型
+    user_id = db.Column(db.Integer, db.ForeignKey('accounts_user.id'))
+    # 建立用户模型表与用户详细信息表一对一关系
+    user = db.relationship('User', backref=db.backref('profile', uselist=False))
+
+
+class UserLoginHistory(db.Model):
+    """ 用户登录历史 """
+    __tablename__ = 'accounts_user_login_history'
+    # 主键
+    id = db.Column(db.Integer, primary_key=True)
+    # 用户名
+    username = db.Column(db.String(64), nullable=False)
+    # 账号平台
+    login_type = db.Column(db.String(32))
+    # IP地址
+    ip = db.Column(db.String(32))
+    # user-agent
+    ua = db.Column(db.String(128))
+    # 创建时间
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    # 最后修改时间
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    # 外键-关联用户模型
+    user_id = db.Column(db.Integer, db.ForeignKey('accounts_user.id'))
+    # 建立用户模型表与用户登录历史表一对多关系
+    user = db.relationship('User', backref=db.backref('login_history_list', lazy='dynamic'))
+
+
+class Question(db.Model):
+    """ 问题 """
+    __tablename__ = 'qa_question'
+    # 主键
+    id = db.Column(db.Integer, primary_key=True)
+    # 问题标题
+    title = db.Column(db.String(128), nullable=False)
+    # 问题描述
+    desc = db.Column(db.String(256))
+    # 问题图片
+    img = db.Column(db.String(256))
+    # 问题详情
+    content = db.Column(db.Text, nullable=False)
+    # 浏览人数
+    view_count = db.Column(db.Integer, default=0)
+    # 逻辑删除
+    is_valid = db.Column(db.Boolean, default=True)
+    # 排序
+    reorder = db.Column(db.Integer, default=0)
+    # 创建时间
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    # 最后修改时间
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    # 外键-关联用户模型
+    user_id = db.Column(db.Integer, db.ForeignKey('accounts_user.id'))
+    # 建立用户模型表与问题表的一对多关系
+    user = db.relationship('User', backref=db.backref('question_list', lazy='dynamic'))
+
+
+class QuestionTags(db.Model):
+    """ 问题标签 """
+    __tablename__ = 'qa_question_tags'
+    # 主键
+    id = db.Column(db.Integer, primary_key=True)
+    # 标签名称
+    tag_name = db.Column(db.String(16), nullable=False)
+    # 逻辑删除
+    is_valid = db.Column(db.Boolean, default=True)
+    # 创建时间
+    created_at = db.Column(db.DateTime, default=datetime.now)
+
+    # 外键-关联问题
+    q_id = db.Column(db.Integer, db.ForeignKey('qa_question.id'))
+    # 建立问题标签表与问题表的一对多关系
+    question = db.relationship('Question', backref=db.backref('tag_list', lazy='dynamic'))
+
+
+class Answer(db.Model):
+    """  回答 """
+    __tablename__ = 'qa_answer'
+    # 主键
+    id = db.Column(db.Integer, primary_key=True)
+    # 回答的内容详情
+    content = db.Column(db.Text, nullable=False)
+    # 逻辑删除
+    is_valid = db.Column(db.Boolean, default=True)
+    # 创建时间
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    # 最后修改时间
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+
+    # 外键-关联用户模型
+    user_id = db.Column(db.Integer, db.ForeignKey('accounts_user.id'))
+    # 外键-关联问题
+    q_id = db.Column(db.Integer, db.ForeignKey('qa_question.id'))
+    # 建立回答表与用户模型表的一对多关系
+    user = db.relationship('User', backref=db.backref('answer_list', lazy='dynamic'))
+    # 建立回答表与问题表的一对多关系
+    question = db.relationship('Question', backref=db.backref('answer_list', lazy='dynamic'))
+
+
+class AnswerComment(db.Model):
+    """ 评论 """
+    __tablename__ = 'qa_answer_comment'
+    # 主键
+    id = db.Column(db.Integer, primary_key=True)
+    # 评论内容
+    content = db.Column(db.String(512), nullable=False)
+    # 赞同人数
+    love_count = db.Column(db.Integer, default=0)
+    # 评论是否公开
+    is_public = db.Column(db.Boolean, default=True)
+    # 逻辑删除
+    is_valid = db.Column(db.Boolean, default=True)
+    # 创建时间
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    # 最后修改时间
+    updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
+    # 回复ID
+    reply_id = db.Column(db.Integer, db.ForeignKey('qa_answer_comment.id'), nullable=True)
+
+    # 外键-关联用户模型
+    user_id = db.Column(db.Integer, db.ForeignKey('accounts_user.id'))
+    # 外键-关联答案
+    answer_id = db.Column(db.Integer, db.ForeignKey('qa_answer.id'))
+    # 外键-关联问题
+    q_id = db.Column(db.Integer, db.ForeignKey('qa_question.id'))
+    # 建立评论表与用户模型表的一对多关系
+    user = db.relationship('User', backref=db.backref('answer_comment_list', lazy='dynamic'))
+    # 建立评论表与答案表的一对多关系
+    answer = db.relationship('Answer', backref=db.backref('answer_comment_list', lazy='dynamic'))
+    # 建立评论表与问题表的一对多关系
+    question = db.relationship('Question', backref=db.backref('question_comment_list', lazy='dynamic'))
+
+
+class AnswerLove(db.Model):
+    """ 回答点赞 """
+    __tablename__ = 'qa_answer_love'
+    # 主键
+    id = db.Column(db.Integer, primary_key=True)
+    # 创建时间
+    created_at = db.Column(db.DateTime, default=datetime.now)
+
+    # 外键-关联用户模型
+    user_id = db.Column(db.Integer, db.ForeignKey('accounts_user.id'))
+    # 外键-关联答案
+    answer_id = db.Column(db.Integer, db.ForeignKey('qa_answer.id'))
+    # 外键-关联问题
+    q_id = db.Column(db.Integer, db.ForeignKey('qa_question.id'))
+    # 建立回答点赞表与用户模型表的一对多关系
+    user = db.relationship('User', backref=db.backref('answer_love_list', lazy='dynamic'))
+    # 建立回答点赞表与答案表的一对多关系
+    answer = db.relationship('Answer', backref=db.backref('answer_love_list', lazy='dynamic'))
+    # 建立回答点赞表与问题表的一对多关系
+    question = db.relationship('Question', backref=db.backref('question_love_list', lazy='dynamic'))
+
+
+class AnswerCollect(db.Model):
+    """ 收藏回答 """
+    __tablename__ = 'qa_answer_collect'
+    # 主键
+    id = db.Column(db.Integer, primary_key=True)
+    # 创建时间
+    created_at = db.Column(db.DateTime, default=datetime.now)
+    # 逻辑删除
+    is_valid = db.Column(db.Boolean, default=True)
+
+    # 外键-关联用户模型
+    user_id = db.Column(db.Integer, db.ForeignKey('accounts_user.id'))
+    # 外键-关联问题
+    q_id = db.Column(db.Integer, db.ForeignKey('qa_question.id'))
+    # 外键-关联答案
+    answer_id = db.Column(db.Integer, db.ForeignKey('qa_answer.id'))
+    # 建立收藏回答表与用户模型表的一对多关系
+    user = db.relationship('User', backref=db.backref('answer_collect_list', lazy='dynamic'))
+    # 建立收藏回答表与问题表的一对多关系
+    question = db.relationship('Question', backref=db.backref('question_collect_list', lazy='dynamic'))
+    # 建立收藏回答表与答案表的一对多关系
+    answer = db.relationship('Answer', backref=db.backref('answer_collect_list', lazy='dynamic'))
+
+
+class QuestionFollow(db.Model):
+    """ 关注问题 """
+    __tablename__ = 'qa_question_follow'
+    # 主键
+    id = db.Column(db.Integer, primary_key=True)
+    # 创建时间
+    created_at = db.Column(db.DateTime)
+    # 逻辑删除
+    is_valid = db.Column(db.Boolean, default=True, comment='逻辑删除')
+
+    # 外键-关联用户模型
+    user_id = db.Column(db.Integer, db.ForeignKey('accounts_user.id'))
+    # 外键-关联问题
+    q_id = db.Column(db.Integer, db.ForeignKey('qa_question.id'))
+    # 建立关注问题表与用户模型表的一对多关系
+    user = db.relationship('User', backref=db.backref('question_follow_list', lazy='dynamic'))
+    # 建立关注问题表与问题表的一对多关系
+    question = db.relationship('Question', backref=db.backref('question_follow_list', lazy='dynamic'))
 
 
 @app.route('/')
---
+++ Flask/qa_flask/constants.py
@@ -0,0 +1,20 @@
+""" 常量配置 """
+from enum import Enum
+
+
+class UserStatus(Enum):
+    """ 用户状态 """
+    # 启用，可登录系统
+    USER_ACTIVE = 1
+    # 禁用，不可登录系统
+    USER_IN_ACTIVE = 0
+
+
+class UserRole(Enum):
+    """ 用户角色 """
+    # 普通用户
+    COMMON = 0
+    # 管理员
+    ADMIN = 1
+    # 超级管理员
+    SUPER_ADMIN = 2
