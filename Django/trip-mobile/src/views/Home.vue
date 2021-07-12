ec9cc313630469252066ca355e53361ae0c94572
smallrookie <1543666575@qq.com>
2021-07-12 22:24:48 +0800

    axios异步请求库

--- Django/trip-mobile/src/views/Home.vue
+++ Django/trip-mobile/src/views/Home.vue
@@ -6,9 +6,20 @@
 
 <script>
 // @ is an alias to /src
+import axios from 'axios'
 
 export default {
-  name: "Home",
+  name: 'Home',
   components: {},
-};
+  created () {
+    axios
+      .get('http://django.t.mukewang.com/api/system/slider/list/?types=11')
+      .then((resp) => {
+        console.log('返回结果为: ', resp)
+      })
+      .catch((err) => {
+        console.log('请求错误: ', err)
+      })
+  }
+}
 </script>
