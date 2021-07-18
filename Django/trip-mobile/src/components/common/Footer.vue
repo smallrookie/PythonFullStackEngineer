e98243c16ab9decefff8ea16a26d88b800f4429c
smallrookie <1543666575@qq.com>
2021-07-18 21:26:51 +0800

    页面底部组件开发

--- Django/trip-mobile/src/components/common/Footer.vue
+++ Django/trip-mobile/src/components/common/Footer.vue
@@ -1,4 +1,19 @@
 <template>
   <!-- 底部导航 -->
-  <div></div>
+  <div>
+    <van-tabbar v-model="active">
+      <van-tabbar-item name="home" icon="wap-home-o">首页</van-tabbar-item>
+      <van-tabbar-item name="search" icon="search">搜索</van-tabbar-item>
+      <van-tabbar-item name="mine" icon="user-o">我的</van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
+<script>
+export default {
+  data () {
+    return {
+      active: 'home'
+    }
+  }
+}
+</script>
--- Django/trip-mobile/src/views/Home.vue
+++ Django/trip-mobile/src/views/Home.vue
@@ -1,4 +1,4 @@
-<template>
+s<template>
   <div class="page-home">
     <!--  Banner图  -->
     <Banner />
@@ -6,6 +6,8 @@
     <Hot />
     <!-- 精选景点 -->
     <Fine />
+    <!-- 底部导航栏 -->
+    <trip-footer />
   </div>
 </template>
 
@@ -13,6 +15,7 @@
 import Banner from '@/components/home/Banner'
 import Hot from '@/components/home/Hot'
 import Fine from '@/components/home/Fine'
+import TripFooter from '@/components/common/Footer'
 
 export default {
   name: 'Home',
@@ -22,7 +25,9 @@ export default {
     // 热门推荐景点
     Hot,
     // 精选景点
-    Fine
+    Fine,
+    // 底部导航栏
+    TripFooter
   },
   created () {}
 }
