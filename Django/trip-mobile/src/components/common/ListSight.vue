74e5a6d15969f478d3c27a03fedc9eb8f2129625
smallrookie <1543666575@qq.com>
2021-07-18 14:48:50 +0800

    景点列表组件开发

---
+++ Django/trip-mobile/src/components/common/ListSight.vue
@@ -0,0 +1,63 @@
+<template>
+  <a href="#" class="sight-item">
+    <img src="/static/home/hot/h1.jpg" alt="">
+    <div class="right">
+      <h5>{{item.name}}</h5>
+      <van-rate v-model="item.score" readonly />
+      <div class="tips">4人点评 | 100%满意</div>
+      <div class="tips light">广东-广州</div>
+      <div class="line-price">￥ {{item.price}} 起</div>
+    </div>
+  </a>
+</template>
+<script>
+export default {
+  props: ['item']
+}
+</script>
+<style lang="less">
+  .sight-item {
+    display: flex;
+    margin-top: 10px;
+    border-bottom: 1px solid #f6f6f6;
+
+    img {
+      width: 100px;
+      height: 100px;
+    }
+
+    h5 {
+      color: #212121;
+      font-size: 14px;
+      margin: 0;
+      padding: 5px 0;
+    }
+
+    .right {
+      flex-grow: 1;
+      text-align: left;
+      justify-content: left;
+      padding-left: 5px;
+      position: relative;
+    }
+
+    .line-price {
+      position: absolute;
+      right: 10px;
+      top: 20px;
+      display: inline-block;
+      color: #f50;
+      font-size: 16px;
+      font-weight: bold;
+    }
+
+    .tips {
+      font-size: 12px;
+      color: #666;
+
+      &.light {
+        color: #999;
+      }
+    }
+  }
+</style>
--- Django/trip-mobile/src/components/home/Fine.vue
+++ Django/trip-mobile/src/components/home/Fine.vue
@@ -6,86 +6,84 @@
     <!--  // 顶部导航  -->
     <!-- 景点列表 -->
     <div class="box-main">
-      <a href="#" class="sight-item" v-for="item in dataList" :key="item.id">
-        <img src="/static/home/hot/h1.jpg" alt="">
-        <div class="right">
-          <h5>{{item.name}}</h5>
-          <van-rate v-model="item.score" readonly />
-          <div class="tips">4人点评 | 100%满意</div>
-          <div class="tips light">广东-广州</div>
-          <div class="line-price">￥ {{item.price}} 起</div>
-        </div>
-      </a>
+      <sight-item v-for="item in dataList" :key="item.id" :item="item"></sight-item>
     </div>
     <!-- // 景点列表 -->
   </div>
 </template>
 <script>
+import SightItem from '@/components/common/ListSight.vue'
 export default {
+  components: {
+    SightItem
+  },
   data () {
     return {
-      dataList: [{
-        id: 1,
-        name: '景点名称',
-        score: 4.5,
-        price: 98
-      },
-      {
-        id: 2,
-        name: '景点名称',
-        score: 4.5,
-        price: 98
-      },
-      {
-        id: 3,
-        name: '景点名称',
-        score: 3,
-        price: 98
-      },
-      {
-        id: 4,
-        name: '景点名称',
-        score: 5,
-        price: 98
-      },
-      {
-        id: 5,
-        name: '景点名称',
-        score: 4.5,
-        price: 98
-      },
-      {
-        id: 6,
-        name: '景点名称',
-        score: 4.5,
-        price: 98
-      },
-      {
-        id: 7,
-        name: '景点名称',
-        score: 4.5,
-        price: 98
-      },
-      {
-        id: 8,
-        name: '景点名称',
-        score: 4.5,
-        price: 98
-      },
-      {
-        id: 9,
-        name: '景点名称',
-        score: 4.5,
-        price: 98
-      },
-      {
-        id: 10,
-        name: '景点名称',
-        score: 4.5,
-        price: 98
-      }
-      ]
+      dataList: []
     }
+  },
+  created () {
+    this.dataList = [{
+      id: 1,
+      name: '景点名称',
+      score: 4.5,
+      price: 98
+    },
+    {
+      id: 2,
+      name: '景点名称',
+      score: 4.5,
+      price: 98
+    },
+    {
+      id: 3,
+      name: '景点名称',
+      score: 3,
+      price: 98
+    },
+    {
+      id: 4,
+      name: '景点名称',
+      score: 5,
+      price: 98
+    },
+    {
+      id: 5,
+      name: '景点名称',
+      score: 4.5,
+      price: 98
+    },
+    {
+      id: 6,
+      name: '景点名称',
+      score: 4.5,
+      price: 98
+    },
+    {
+      id: 7,
+      name: '景点名称',
+      score: 4.5,
+      price: 98
+    },
+    {
+      id: 8,
+      name: '景点名称',
+      score: 4.5,
+      price: 98
+    },
+    {
+      id: 9,
+      name: '景点名称',
+      score: 4.5,
+      price: 98
+    },
+    {
+      id: 10,
+      name: '景点名称',
+      score: 4.5,
+      price: 98
+    }
+    ]
   }
 }
 </script>
@@ -97,51 +95,6 @@ export default {
       padding: 10px 0;
     }
 
-    .box-main {
-      .sight-item {
-        display: flex;
-        margin-top: 10px;
-        border-bottom: 1px solid #f6f6f6;
-
-        img {
-          width: 100px;
-          height: 100px;
-        }
-
-        h5 {
-          color: #212121;
-          font-size: 14px;
-          margin: 0;
-          padding: 5px 0;
-        }
-
-        .right {
-          flex-grow: 1;
-          text-align: left;
-          justify-content: left;
-          padding-left: 5px;
-          position: relative;
-        }
-
-        .line-price {
-          position: absolute;
-          right: 10px;
-          top: 20px;
-          display: inline-block;
-          color: #f50;
-          font-size: 16px;
-          font-weight: bold;
-        }
-
-        .tips {
-          font-size: 12px;
-          color: #666;
-
-          &.light {
-            color: #999;
-          }
-        }
-      }
-    }
+    .box-main {}
   }
 </style>
